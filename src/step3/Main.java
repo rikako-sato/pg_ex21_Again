97e36cb9f716f7f0576958078d3070b5ac6dd5bd
z2160040 <z2160040@user-PC>
2017-10-15 17:46:42 +0900

    電話番号の保存方法を配列に変更/step3

---
+++ src/step3/Main.java
@@ -0,0 +1,69 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter("data/invoice.dat"));
+		BufferedReader reader = new BufferedReader(new FileReader("data/record.log"));
+
+		String ownerTelNumber = null;
+		int basicCharge = 0; // 基本料金
+		int unitPrice = 0; // 通話単価
+		int callCharge = 0; // 通話料金
+		DayService dayService = new DayService();
+		FamilyService familyService = new FamilyService();
+
+		String line = reader.readLine();
+		while (line != null) {
+			char code = line.charAt(0);
+
+			switch (code) {
+			case '1':
+				basicCharge = 1000; // 基本料金を初期化する
+				unitPrice = 20; // 通話単価を初期化する
+				callCharge = 0; // 通話料金を初期化する
+				dayService.clear(); // 昼トク割引サービスを初期化する
+				familyService.clear();//家族割引サービスを初期化する
+
+				ownerTelNumber = line.substring(2);
+				break;
+
+			case '2':
+				dayService.checkService(line);
+				familyService.checkService(line);
+				familyService.familyNum(line);
+				break;
+
+			case '5':
+				int calcUnitPrice = dayService.calcUnitPrice(line, unitPrice); // 通話単価を求める
+				int calcLastPrice = familyService.calcLastPrice(line,calcUnitPrice);//通話単価を求める（Ｅ１）
+				int callTime = Integer.parseInt(line.substring(19, 21));
+				callCharge += calcLastPrice * callTime;
+
+
+
+				break;
+
+			case '9':
+				basicCharge = dayService.calcBasicCharge(basicCharge)+familyService.calcBasicCharge(basicCharge); // 基本料金を求める
+
+				writer.write("1 " + ownerTelNumber + "\n");
+				writer.write("5 " + basicCharge + "\n");
+				writer.write("7 " + callCharge + "\n");
+				writer.write("9 ====================\n");
+				break;
+			}
+
+			line = reader.readLine();
+		}
+		reader.close();
+		writer.close();
+	}
+
+}
---
+++ src/step3/RecordTest.java
@@ -0,0 +1,55 @@
+package step3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RecordTest {
+
+	@Test
+	public void RecordCodeTest() {
+		Record record = new Record("2 C1 090-1234-0002");
+		assertEquals('2', record.getRecordCode());
+	}
+
+	@Test
+	public void OwnerTelNumberTest() {
+		Record record = new Record("1 090-1234-0001");
+		assertEquals("090-1234-0001", record.getOwnerTelNumber());
+	}
+
+	@Test
+	public void ServiceCodeTest() {
+		Record record = new Record("2 C1 090-1234-0002");
+		assertEquals("C1", record.getServiceCode());
+	}
+
+	@Test
+	public void ServiceOptionTest1() {
+		Record record = new Record("2 C1 090-1234-0002");
+		assertEquals("090-1234-0002", record.getServiceOption());
+	}
+	@Test
+	public void ServiceOptionTest2() {
+		Record record = new Record("2 E1");
+		assertNull(record.getServiceOption());
+	}
+
+	@Test
+	public void getStartHourTest() {
+		Record record = new Record("5 2004/06/06 12:34 007 090-1234-0003");
+		assertEquals(12, record.getStartHour());
+	}
+
+	@Test
+	public void CallMinutesTest() {
+		Record record = new Record("5 2004/06/06 12:34 007 090-1234-0003");
+		assertEquals(7, record.getCallMinutes());
+	}
+
+	@Test
+	public void CallNumberTest() {
+		Record record = new Record("5 2004/06/06 12:34 007 090-1234-0003");
+		assertEquals("090-1234-0003", record.getCallNumber());
+	}
+}
